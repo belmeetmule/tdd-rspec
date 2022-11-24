218450866607ed821a4c6ad6b886897a9df4167d
iAmWaris97 <haleem.waris@gmail.com>
2022-11-25 03:11:40 +0430

    Resolve linter errors

--- solver.rb
+++ solver.rb
@@ -1,21 +1,23 @@
 class Solver
-    def factorial(number)
-      number.negative? ? (raise StandardError, 'NegativeNumberFactorialError') :
-       (number == 0 ? 1 : (number * factorial(number -1)))
-    end
-  
-    def reverse(word)
-      word.instance_of?(String) ? word.reverse : (raise StandardError, 'Not A String Error')
-    end
-    def fizzbuzz(number)
-      if (number % 15).zero?
-        'fizzbuzz'
-      elsif (number % 3).zero?
-        'fizz'
-      elsif (number % 5).zero?
-        'buzz'
-      else
-        number.to_s
-      end
+  def factorial(number)
+    raise StandardError, 'NegativeNumberFactorialError' if number.negative?
+
+    (number.zero? ? 1 : (number * factorial(number - 1)))
+  end
+
+  def reverse(word)
+    word.instance_of?(String) ? word.reverse : (raise StandardError, 'Not A String Error')
+  end
+
+  def fizzbuzz(number)
+    if (number % 15).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
     end
   end
+end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,35 +1,35 @@
 require_relative '../solver'
 # Solver class tests
 describe Solver do
-    context 'when solver object is instantiated' do
-        it 'creates a Solver object' do
-            solver = Solver.new
-            expect(solver).to be_kind_of(Solver)
-        end
+  context 'when solver object is instantiated' do
+    it 'creates a Solver object' do
+      solver = Solver.new
+      expect(solver).to be_kind_of(Solver)
+    end
+  end
+
+  context 'factorial method' do
+    # Checkes whether a solver class responds to the factorial message (has a factorial method)
+    it 'factorial method should exist/respond' do
+      solver = Solver.new
+      expect(solver).to respond_to(:factorial)
     end
 
-    context 'factorial method' do
-        # Checkes whether a solver class responds to the factorial message (has a factorial method)
-        it 'factorial method should exist/respond' do
-            solver = Solver.new
-            expect(solver).to respond_to(:factorial)
-        end
-
-        it 'it should return factorieal of N if N is positive ' do
-            solver = Solver.new
-            expect(solver.factorial(4)).to eq 24
-        end
-
-        it 'it should raise error if -ve number is given' do
-            solver = Solver.new
-            expect do
-              solver.factorial(-1)
-            end.to raise_error('NegativeNumberFactorialError')
-          end
+    it 'it should return factorieal of N if N is positive ' do
+      solver = Solver.new
+      expect(solver.factorial(4)).to eq 24
     end
-    # reverse method tests
-    context 'When reverse method is called' do
-        solver = Solver.new
+
+    it 'it should raise error if -ve number is given' do
+      solver = Solver.new
+      expect do
+        solver.factorial(-1)
+      end.to raise_error('NegativeNumberFactorialError')
+    end
+  end
+  # reverse method tests
+  context 'When reverse method is called' do
+    solver = Solver.new
     it 'should return the reversed string' do
       expect(solver.reverse('hello')).to match('olleh')
     end
@@ -41,21 +41,21 @@ describe Solver do
         expect(solver.reverse(5))
       end.to raise_error('Not A String Error')
     end
-    #fizzbuzz tests
+    # fizzbuzz tests
     context 'When fizzbuzz method is called' do
       solver = Solver.new
-    it 'should return fizz when N is divisible by 3' do
-      expect(solver.fizzbuzz(6)).to match('fizz')
-    end
-    it 'should return buzz when N is divisible by 5' do
-      expect(solver.fizzbuzz(10)).to match('buzz')
-    end
-    it 'should return fizzbuzz when N is divisible by 3 and 5' do
-      expect(solver.fizzbuzz(15)).to match('fizzbuzz')
+      it 'should return fizz when N is divisible by 3' do
+        expect(solver.fizzbuzz(6)).to match('fizz')
       end
-    it 'should return \'N\' when N doesn\'t match the above conditions' do
-      expect(solver.fizzbuzz(22)).to match('22')
-     end
+      it 'should return buzz when N is divisible by 5' do
+        expect(solver.fizzbuzz(10)).to match('buzz')
+      end
+      it 'should return fizzbuzz when N is divisible by 3 and 5' do
+        expect(solver.fizzbuzz(15)).to match('fizzbuzz')
+      end
+      it 'should return \'N\' when N doesn\'t match the above conditions' do
+        expect(solver.fizzbuzz(22)).to match('22')
+      end
     end
   end
 end
