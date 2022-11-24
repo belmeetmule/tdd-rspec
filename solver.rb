1a557bf2f93b7797625fcad65b00aabf45438953
iAmWaris97 <haleem.waris@gmail.com>
2022-11-25 03:10:50 +0430

    add fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -7,4 +7,15 @@ class Solver
     def reverse(word)
       word.instance_of?(String) ? word.reverse : (raise StandardError, 'Not A String Error')
     end
+    def fizzbuzz(number)
+      if (number % 15).zero?
+        'fizzbuzz'
+      elsif (number % 3).zero?
+        'fizz'
+      elsif (number % 5).zero?
+        'buzz'
+      else
+        number.to_s
+      end
+    end
   end
